2979c01235f4dfa64bb9bd1d820fed3e522680db
Maxfees <mbillah.cse.sust20@gmail.com>
2025-06-10 06:39:15 +0600

    ai counselor created

---
+++ Backend/controller/AiCounselorController.js
@@ -0,0 +1,6 @@
+const Doctor = require("../model/models").doctorSchemaExp;
+const Groq = require("groq-sdk");
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY_THREE });
+const Assessment = require("../model/models").Assessment; 
+const MoodEntry = require("../model/models").MoodEntry; 
+const ActivityEntry = require("../model/models").ActivityEntry; 
